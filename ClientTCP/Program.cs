6289218e66e2898c23f5284ffb93a5da7b360cb3
Daniil Lautenshleger <alximik683@gmail.com>
2019-11-09 03:02:58 +0300

    first commit

---
+++ ClientTCP/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region TCPCLient
+            /*
+            const string ip_client = "127.0.0.1";
+            const int port_client = 8080;
+
+            var TCPEndPoint = new IPEndPoint(IPAddress.Parse(ip_client), port_client);
+
+            var TCP_Socket_client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Write your message...");
+            var message = Console.ReadLine();
+
+            var data = Encoding.UTF8.GetBytes(message);
+
+            TCP_Socket_client.Connect(TCPEndPoint);
+
+            TCP_Socket_client.Send(data);
+
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+
+            do
+            {
+                size = TCP_Socket_client.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            } while (TCP_Socket_client.Available > 0);
+
+            Console.WriteLine(answer);
+
+            TCP_Socket_client.Shutdown(SocketShutdown.Both);
+            TCP_Socket_client.Close();
+
+            Console.ReadLine();*/
+            #endregion
+
+            #region UDPClient
+
+            //const string ip_client = "127.0.0.1";
+            //const int port_client = 8082;
+
+            //var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip_client), port_client);
+
+            //var udp_client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //udp_client_socket.Bind(udpEndPoint);
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Write your message...");
+            //    var message = Console.ReadLine();
+
+            //    EndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse(ip_client), 8081);
+            //    udp_client_socket.SendTo(Encoding.UTF8.GetBytes(message),serverEndpoint);
+
+            //    var buffer = new byte[256];
+            //    var size = 0;
+            //    var data = new StringBuilder();
+
+            //    EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            //    do
+            //    {
+            //        size = udp_client_socket.ReceiveFrom(buffer, ref serverEndpoint);
+            //        data.Append(Encoding.UTF8.GetString(buffer));
+            //    } while (udp_client_socket.Available > 0);
+
+            //    Console.WriteLine(data);
+            //    Console.ReadLine();
+            //}
+            #endregion
+
+            const string  ip_address_client = "127.0.0.1";
+            const int port = 904;
+
+            while (true)
+            {
+                IPEndPoint end_client = new IPEndPoint(IPAddress.Parse(ip_address_client), port);
+
+                Socket socket_client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                Console.WriteLine("Поддержка вас слышит, введите вашу претензию/предложение!");
+                string message = Console.ReadLine();
+
+                var data = Encoding.UTF8.GetBytes(message);
+                socket_client.Connect(end_client);
+                socket_client.Send(data);
+
+                byte[] buffer = new byte[256]; // массив байт куда будем загружать данные, которые принял сокет выше
+                int size = 0; // вычислим размер полученного сообщения, чтоб не тратить лишнюю память
+                StringBuilder answer = new StringBuilder(); // строка, в которую поместим полученное раскодированное сообщение
+
+                try
+                {
+                    do
+                    {
+                        size = socket_client.Receive(buffer); // узнаем размер полученной информации
+                        answer.Append(Encoding.UTF8.GetString(buffer, 0, size)); // переносим информацию в строку для работы с ней
+
+                    } while (socket_client.Available > 0); // пока полученное сообщение не передалось полностью
+
+                    Console.WriteLine(answer);
+
+                    socket_client.Shutdown(SocketShutdown.Both);
+                    socket_client.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Socket/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ServerTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string address_support = "127.0.0.1"; // адрес поддержки(сервера)
+            const int port_support = 904; // пор поддержки(сервера)
+
+            IPEndPoint support_endpoint = new IPEndPoint(IPAddress.Parse(address_support),port_support); // Точка подключения,нужна по сути для паузы
+
+            var Socket_support = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp); // Сам сокет, штука - посредник между сервером и протоколом(TCP/UDP)
+
+            Socket_support.Bind(support_endpoint); // Связываем сокет сервера с точкой подключения
+            Socket_support.Listen(5); // Режим ожидания, слушает конкретно данный ендпоинт(указанный в связке сверху)
+            Socket lisntener;
+            bool first = true;
+
+            while (true)
+            {
+                try
+                {
+                    lisntener = Socket_support.Accept(); // создаем новый сокет для принятия информации от клиента
+                    byte[] buffer = new byte[256]; // массив байт куда будем загружать данные, которые принял сокет выше
+                    int size = 0; // вычислим размер полученного сообщения, чтоб не тратить лишнюю память
+                    StringBuilder data = new StringBuilder(); // строка, в которую поместим полученное раскодированное сообщение
+                    do
+                    {
+                        size = lisntener.Receive(buffer); // узнаем размер полученной информации
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size)); // переносим информацию в строку для работы с ней
+
+                    } while (lisntener.Available > 0); // пока полученное сообщение не передалось полностью
+
+                    Console.WriteLine(data); // пишем полученное сообщение в консоль
+                    if (first)
+                    {
+                        lisntener.Send(Encoding.UTF8.GetBytes("Добрый день, вас приветствует поддержка такая-то! \n"));
+                        first = false;
+                    }
+                    lisntener.Send(Encoding.UTF8.GetBytes("Ваше сообщение доставлено, ожидайте ответа \n")); // Отправляем клиенту информации об успешной передачи
+                    Regex reg = new Regex(@"\w*проблема\w*", RegexOptions.Compiled | RegexOptions.IgnoreCase); // Регулярное выражение для поиска совпадений
+                    MatchCollection matches = reg.Matches(data.ToString()); // собственно узнаем есть ли совпадения
+                    if (matches.Count > 0) // если есть отправляем нужную информацию
+                    {
+                        lisntener.Send(Encoding.UTF8.GetBytes("В чём ваша проблема?опишите \n")); // отправка
+                    }
+                    reg = new Regex(@"\w*соединение\w*", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                    matches = reg.Matches(data.ToString());
+                    if (matches.Count > 0)
+                    {
+                        lisntener.Send(Encoding.UTF8.GetBytes("Если у вас проблема с соединением попробуйте перезайти ещё раз \n")); // отправка
+                    }
+
+                    lisntener.Shutdown(SocketShutdown.Both); //  выключаем передачу и приём информации у сокета, который принимает инфу
+                    lisntener.Close(); // закрываем соединение
+                }
+                catch (Exception e) // отлов ошибок
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+        
+        }
+
+
+     }
+
+}
+
+
+#region TCPServer
+/*
+const string ip_server = "127.0.0.1";
+const int port_server = 8080;
+
+var TCPEndPoint = new IPEndPoint(IPAddress.Parse(ip_server),port_server);
+
+var TCP_server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+TCP_server_socket.Bind(TCPEndPoint);
+TCP_server_socket.Listen(6);
+
+while (true)
+{
+    var listener = TCP_server_socket.Accept();
+    var buffer = new byte[256];
+    var size = 0;
+    var data = new StringBuilder();
+
+    do
+    {
+        size = listener.Receive(buffer);
+        data.Append(Encoding.UTF8.GetString(buffer,0,size));
+
+    } while (listener.Available > 0);
+
+    Console.WriteLine(data.ToString());
+
+    listener.Send(Encoding.UTF8.GetBytes("Success"));
+
+    listener.Shutdown(SocketShutdown.Both);
+    listener.Close(); 
+    }*/
+#endregion
+
+#region UDPServer
+//const string ip_server = "127.0.0.1";
+//const int port_server = 8081;
+
+//var udpEndPoint = new IPEndPoint(IPAddress.Parse(ip_server), port_server);
+
+//var udp_server_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+//udp_server_socket.Bind(udpEndPoint);
+
+//while (true)
+//{
+//    var buffer = new byte[256];
+//    var size = 0;
+//    var data = new StringBuilder();
+
+//    EndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+//    do
+//    {
+//        size = udp_server_socket.ReceiveFrom(buffer, ref senderEndPoint);
+//        data.Append(Encoding.UTF8.GetString(buffer));
+//    } while (udp_server_socket.Available > 0);
+
+//    udp_server_socket.SendTo(Encoding.UTF8.GetBytes("Success"),senderEndPoint);
+
+//    Console.WriteLine(data);
+//}
+#endregion
+
+
